b8a9c075f9d18429f4946078b873ab803dec5239
root <root@ubuntu.members.linode.com>
2016-09-10 21:24:58 +0000

    added the results from barry's speech

--- sphinx.sh
+++ sphinx.sh
@@ -1,3 +1,3 @@
-pocketsphinx_continuous -infile ~/vox/split-wav/goforward.raw -hmm ~/install-cmu/pocketsphinx-5prealpha/model/en-us/en-us \
+pocketsphinx_continuous -infile ~/vox/Barry/output.raw -hmm ~/install-cmu/pocketsphinx-5prealpha/model/en-us/en-us \
                         -allphone ~/install-cmu/pocketsphinx-5prealpha/model/en-us/en-us/en-us-phone.lm.bin -backtrace yes \
                         -beam 1e-20 -pbeam 1e-20 -lw 2.0
