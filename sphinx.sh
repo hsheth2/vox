c0b0d6a36a7d20d5b7e53a49eff16d61a251ba03
root <root@ubuntu.members.linode.com>
2016-09-10 22:04:59 +0000

    fix sphinx script

--- sphinx.sh
+++ sphinx.sh
@@ -1,3 +1,2 @@
-pocketsphinx_continuous -infile ~/vox/Barry/output.raw -hmm ~/install-cmu/pocketsphinx-5prealpha/model/en-us/en-us \
-                        -allphone ~/install-cmu/pocketsphinx-5prealpha/model/en-us/en-us/en-us-phone.lm.bin -backtrace yes \
-                        -beam 1e-20 -pbeam 1e-20 -lw 2.0
+gcc -o phoneme_reader phoneme_reader.c -DMODELDIR=\"`pkg-config --variable=modeldir pocketsphinx`\" `pkg-config --cflags --libs pocketsphinx sphinxbase`
+
